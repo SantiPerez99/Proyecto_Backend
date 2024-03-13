f7465f0a1521ee8c91654ece2aaed308d3372c1f
santiperez99 <perezsn99@gmail.com>
2024-03-13 12:01:24 -0300

    subo a mi repositorio el desafio numero 2 aplicado en el proyecto

--- Desafio01/desafioEntregable01.js
+++ Desafio01/desafioEntregable01.js
@@ -1,14 +1,47 @@
 
+//const fs = require('fs');
+import fs from 'fs'
 
 export default class ProducManager {
 
     #products
+    #path;
     static idproducto = 0;
+    
 
     constructor(){
-        this.#products = []
-    }
 
+        this.#path = '../data/productos.json';
+        this.#products = this.buscarProductos();
+    }
+    IdproductoAgregado(){
+        let id = 1
+        if(this.#products.length != 0)
+            id = this.#products[this.#products.length - 1].id + 1;
+        return id;
+        
+    }
+    buscarProductos(){  
+        try{
+            if(fs.existsSync(this.#path)){  // pregunto si el archivo existe en path que es el json
+                return JSON.parse(fs.readFileSync(this.#path,'utf-8'))  // si existe lo parsea 
+            }
+
+            return [];  // si no existe muestra un array vacio 
+
+        } catch (error) {
+            console.log(`ocurrio un error al buscar los productos, ${error}`);
+        }
+    }
+    guardarArchivo(){
+        try{
+            fs.writeFileSync(this.#path, JSON.stringify(this.#products, null, 4)); // null y 4 hacen que el array se vea de forma separada y una abajo del otro
+            console.log('Archivo guardado exitosamente.');
+        } catch (error) {
+            console.log(`Ocurrió un error al guardar el producto: ${error}`);
+        }
+    }
+    
     addProduct( title, description, price, thumbnail, code, stock ){
         
        if(!title || !description || !price || !thumbnail || !code || !stock)
@@ -23,8 +56,8 @@ export default class ProducManager {
 
 
         // id autoincrementar
-        ProducManager.idproducto = ProducManager.idproducto +1
-        const id = ProducManager.idproducto;
+        ProducManager.idproducto++;
+        const id = this.IdproductoAgregado();
         const nuevoProducto = {
             id,
             title,
@@ -35,6 +68,7 @@ export default class ProducManager {
             stock
         };
         this.#products.push(nuevoProducto);
+        this.guardarArchivo();
         return "producto agregado con exito!"
     }
 
@@ -52,6 +86,36 @@ export default class ProducManager {
         else 
              return `Not Found del producto ${id}`  // si no se encuentra la variable retorna not found           
     }
+
+    updateProduct(id, object) {
+        let mensajeError = `EL producto con id ${id} no existe`
+        let mensajeUpdate = `El producto con id ${id} fue actualizado con existo...!!! `
+        const index = this.#products.findIndex(p=> p.id === id);
+
+        if(index >= 0){
+            const {id, ...rest} = object
+            this.#products[index] = {...this.#products[index], ...rest} //sobre un producto (this.#products[index]) aplica las nuevas modificaciones  {...this.#products[index], ...rest}
+            this.guardarArchivo()
+            return mensajeUpdate
+        }
+        return mensajeError
+    }
+    
+    deleteProduct(id){
+        let mensajeError = `EL producto con id ${id} no existe`
+        let mensajeEliminado = `El producto con id ${id} fue eliminado correctamente...!!!`
+        const index = this.#products.findIndex(p=> p.id === id);
+        if(index !== -1 ){
+            this.#products = this.#products.filter(p=> p.id !== id); // filtra todos los elementos del array MENOS el elemento que recibe por id. 
+            this.guardarArchivo();   // guardo el nuevo archivo con el elemento eliminado
+            return mensajeEliminado;
+        }
+        return mensajeError;
+
+    }
+
+
+
 }
 
 
--- Desafio01/testingDesafio01.js
+++ Desafio01/testingDesafio01.js
@@ -1,10 +1,25 @@
 import ProducManager from "./desafioEntregable01.js";
 
 const producto = new ProducManager();
-
-
+//const fs = require('fs');
+import fs from 'fs'
 
 console.log(producto.addProduct('asus','pc',10000,'https://asuspc.com','xax1456',10 ));
 console.log(producto.addProduct('macBook','pc',15000,'https://macBook.com','xax1789',5 ));
-console.log(producto.getProducts());
+console.log(producto.addProduct('macBook','pc',15000,'https://macBook.com','xax1799',5 ));
+
+
+
+// console.log(producto.getProducts());
 // console.log(producto.getProductbyId(1));
+
+// console.log(producto.deleteProduct(3));
+
+const ActualizarProducto = {
+    
+        "price": 15000,
+        "stock": 5
+    
+}
+
+console.log(producto.updateProduct(1, ActualizarProducto));
